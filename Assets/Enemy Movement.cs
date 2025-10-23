b196559e21ded7466ec10632977b1905a68e0ce9
Qwerty121240k <534402@chrome.escg.ac.uk>
2025-10-23 16:24:13 +0100

    ok

--- Assets/Enemy Movement.cs
+++ Assets/Enemy Movement.cs
@@ -5,15 +5,15 @@ using UnityEngine;
 public class EnemyMovement : MonoBehaviour
 {
     public float moveSpeed = 5f;        // Horizontal movement speed
-    public float jumpForce = 10f;       // Force applied when jumping
+          
     public Rigidbody2D rb;              // Reference to Rigidbody2D component
+    private float horizontalInput;
+    public float loop = 5f;
+    public float loop2 = 5f;
 
 
 
-   
 
-  
-   
 
 
     // Start is called before the first frame update
@@ -25,6 +25,25 @@ public class EnemyMovement : MonoBehaviour
     // Update is called once per frame
     void Update()
     {
-        
+        horizontalInput = loop;
+        // Input.GetAxisRaw("Horizontal");
+       
+
+
+        if (horizontalInput > 0)
+        {
+            transform.localScale = new Vector3(-1, 1, 1); // Face left
+        }
+        else if (horizontalInput < 0)
+        {
+            transform.localScale = new Vector3(1, 1, 1); // Face right
+        }
+    }
+
+    void FixedUpdate()
+    {
+        // Apply  movement 
+        rb.velocity = new Vector2(horizontalInput * moveSpeed, rb.velocity.y);
+      
     }
 }
