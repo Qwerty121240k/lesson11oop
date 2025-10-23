e4aa9bc35321d02fcbd708d9ea1d18c9abc190d2
Qwerty121240k <534402@chrome.escg.ac.uk>
2025-10-23 12:14:54 +0100

    ok enemy move

---
+++ Assets/Enemy Movement.cs
@@ -0,0 +1,30 @@
+using System.Collections;
+using System.Collections.Generic;
+using UnityEngine;
+
+public class EnemyMovement : MonoBehaviour
+{
+    public float moveSpeed = 5f;        // Horizontal movement speed
+    public float jumpForce = 10f;       // Force applied when jumping
+    public Rigidbody2D rb;              // Reference to Rigidbody2D component
+
+
+
+   
+
+  
+   
+
+
+    // Start is called before the first frame update
+    void Start()
+    {
+        
+    }
+
+    // Update is called once per frame
+    void Update()
+    {
+        
+    }
+}
